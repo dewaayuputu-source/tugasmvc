0fbd4373fc3fb8b12343c83cc24aa057162b7469
dewaayuputu-source <dewaputuayunew@gmail.com>
2026-04-25 12:00:02 +0700

    Add files via upload

---
+++ MahasiswaController.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "model/mahasiswa.php";
+
+class MahasiswaController {
+    public function index() {
+        $model = new Mahasiswa();
+        $data = $model->getData();
+
+        require "view/mahasiswaview.php";
+    }
+}
+
+?>
